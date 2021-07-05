57abfcb88a3bb78d30c114ba5d2cb7f5a76057d5
Jacob Hildebrandt <JacobHildebrandt@Jacobs-Air.hitronhub.home>
2021-07-05 09:06:46 -0700

    Added Create Warehouse Functionality

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -3,16 +3,32 @@ const knexConfig = require('./../knexfile').development;
 const knex = require('knex')(knexConfig);
 const router = express.Router();
 
-router.route('/').get((req, res) => {
-  knex
-    .select('*')
-    .from('warehouses')
-    .then((data) => {
-      res.status(200).json(data);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
+router
+  .route('/')
+  .get((req, res) => {
+    knex
+      .select('*')
+      .from('warehouses')
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  })
+  .post((req, res) => {
+    console.log(req.body);
+    knex('warehouses')
+      .insert(req.body)
+      .then((data) => {
+        console.log(data);
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+
+router.route('/');
 
 module.exports = router;
